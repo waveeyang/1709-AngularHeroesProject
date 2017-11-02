8208ac3e5316f89edb8dfe98fb6760e8db84772d
waveeyang <waveeyang@gmail.com>
2017-11-02 11:03:22 -0400

    i did it

--- AngularFirstProject/src/app/app.component.ts
+++ AngularFirstProject/src/app/app.component.ts
@@ -18,8 +18,23 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Supercalifragilisticexpialidocious';
-  hero: Hero = {
-    id:1,
-    name:'Illidan'
-  };
+  selectedHero: Hero;
+  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+const HEROES: Hero[] = [
+    { id: 10, name: 'Illidan Stormrage' },
+    { id: 11, name: 'Anakin Skywalker' },
+    { id: 12, name: 'Arthas Menethil' },
+    { id: 13, name: 'Legolas' },
+    { id: 14, name: 'Martin the Warrior' },
+    { id: 15, name: 'Aragorn' },
+    { id: 16, name: 'Batman' },
+    { id: 17, name: 'Wolverine' },
+    { id: 18, name: 'Uchiha Sasuke' },
+    { id: 19, name: 'Uchiha Itachi' },
+    { id: 20, name: 'Gimli' }
+];
